c39a89d0757c16e5a9e6c2303c0be0f21794f923
rexmortus <alecs@eml.cc>
2024-01-17 11:50:40 +1100

    Fix clash with handlebars helper

--- scripts/module.js
+++ scripts/module.js
@@ -2,22 +2,11 @@ import { registerSettings, moduleName } from './settings.js';
 import { TimekeeperApplicationForm } from './lib/forms/TimekeeperApplicationForm/TimekeeperApplicationForm.js'
 import { TimeKeeper } from './lib/timekeeper.js';
 
-// Register handlebars helpers
-Handlebars.registerHelper('isLessThan', function(n1, n2, options) {
-    return (n1 < n2) ? options.fn(this) : options.inverse(this);
-});
-
-Handlebars.registerHelper('isEqualTo', function(n1, n2, options) {
-    return (n1 === n2) ? options.fn(this) : options.inverse(this);
-});
-
 Hooks.once('init', async function() {
 
     // --- Register the settings for the module --- //
     registerSettings();
 
-    // --- Setup the TimeKeeper --- //
-    // Have to load the calendar data here because you can't do await in a constructor??
 
     let calendarReq;
     const calendarName = game.settings.get('vtt-timekeeper', 'calendar');
@@ -39,35 +28,7 @@ Hooks.once('init', async function() {
         mainForm.render(false);
     });
 
-    // --- Register chat journalling functions --- //
-    Hooks.on("chatCommandsReady", commands => {
-
-        // Add a note
-        commands.register({
-            name: "/note",
-            module: "_chatcommands",
-            aliases: ["/n"],
-            description: "Write a note in today's journal",
-            icon: "✎ ",
-            requiredRole: "NONE",
-            callback: function(chat, parameters, messageData) {
-
-                if (parameters) {
-                    let user = game.users.get(messageData.user);
-                    let title = `${user.name} at ${SimpleCalendar.api.currentDateTimeDisplay().time}` 
-                    SimpleCalendar.api.addNote(title, parameters, {}, {}, true, SimpleCalendar.api.NoteRepeat.Never, ['Notes']);
-                    return { content: `added a note to the journal: <blockquote>${parameters}</blockquote>` }
-                } else {
-                    ui.notifications.info("There was no journal content");
-                }
-
-                 
-            },
-            autocompleteCallback: (menu, alias, parameters) => [game.chatCommands.createInfoElement("Write a note in today's journal")],
-            closeOnComplete: true
-        });
-
-    });
+    Hooks.on('vtt-time')
     
     // Opening the window
     game.keybindings.register("show-timekeeper", "show-timekeeper", {
